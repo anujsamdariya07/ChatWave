82b8fa52f6b6cb015e59b68988b50e39a6def3a8
anujsamdariya07 <anujsamdariya07@gmail.com>
2024-07-08 15:37:58 +0530

    frontend router setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
+import React from "react"
+import { createBrowserRouter, createRoutesFromElements, Outlet, Route, RouterProvider } from 'react-router-dom'
+import Join from "./components/Join/Join"
+import Chat from './components/Chat/Chat'
+
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Outlet/>}>
+        <Route path='' element={<Join/>}/>
+        <Route path='chat' element={<Chat/>}/>
+      </Route>
+    )
+  )
+  
   return (
     <>
-      <h1>
-        Hello
-      </h1>
+      <RouterProvider router={router} />
     </>
   )
 }
